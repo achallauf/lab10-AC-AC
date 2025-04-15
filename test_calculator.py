29bd2ee6c396a486e056cbb8827d36bcc2f29117
Aryaan Verma <aryaanverma@Aryaans-MacBook-Pro.local>
2025-04-14 23:32:46 -0400

    Created test cases for the calculator

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,15 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-5, 10), 5)
+        self.assertEqual(add(0, 1), 1)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(5, 10), -5)
+        self.assertEqual(subtract(5, 3), 2)
     # ##########################
 
     ######## Partner 1
@@ -18,22 +21,20 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
     
-    ######## Partner 1
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
+
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(10, 100), 2)
+        self.assertEqual(log(2, 8), 3)
+        self.assertEqual(log(3, 27), 3)
+
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(0, 5)
+
     # def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
